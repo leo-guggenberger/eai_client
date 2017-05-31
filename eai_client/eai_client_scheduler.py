cc1e7b6bbe27274d83a24e195674c3769e357541
leo-guggenberger <info@leo-guggenberger.at>
2017-05-31 11:42:22 +0200

    Update eai_client_scheduler.py

--- eai_client/eai_client_scheduler.py
+++ eai_client/eai_client_scheduler.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from openerp import models
+import xmlrpclib
 
 # EAI Client Scheduler
 class eai_client_scheduler(models.Model):
@@ -11,6 +12,8 @@ class eai_client_scheduler(models.Model):
 
     def run_scheduler(self, cr, uid, context=None):
         company_id = self.pool.get('res.users').browse(cr, uid, uid, context=context).company_id.id
+        eai_server_url = self.pool.get('res.users').browse(cr, uid, uid, context=context).company_id.id
         
         # Step 1: Produkte ausspielen
-        product_ids = self.pool['product.template'].search(cr,uid, ['|',('company_id','=',company_id),('active','=','true'),('sale_ok','=','true')], context=context)
+        #product_ids = self.pool['product.template'].search(cr,uid, ['|',('company_id','=',company_id),('active','=','true'),('sale_ok','=','true')], context=context)
+
