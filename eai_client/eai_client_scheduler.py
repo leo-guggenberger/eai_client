be529ed3a633bdb074306a7259293e0f6f07e5e8
leo-guggenberger <info@leo-guggenberger.at>
2017-05-31 12:01:05 +0200

    Update eai_client_scheduler.py

--- eai_client/eai_client_scheduler.py
+++ eai_client/eai_client_scheduler.py
@@ -19,4 +19,13 @@ class eai_client_scheduler(models.Model):
         
         # Step 1: Produkte ausspielen
         #product_ids = self.pool['product.template'].search(cr,uid, ['|',('company_id','=',company_id),('active','=','true'),('sale_ok','=','true')], context=context)
-
+        eai_server_models = xmlrpclib.ServerProxy('{}/xmlrpc/2/common'.format(eai_server_url))
+        eai_server_uid = eai_server_models.authenticate(eai_server_db, eai_server_user, eai_server_password, {})
+        eai_server_models.execute_kw(eai_server_db, eai_server_uid, eai_server_password,'eai_server.messages', 'check_access_rights', ['create'], {'raise_exception': False})
+        eai_serverid = eai_server_models.execute_kw(eai_server_db, eai_server_uid, eai_server_password, 'eai_server.messages', 'create', [{
+            'name': 'Test',
+            'direction': 'outgoing',
+            'sender_id': 'Sender',
+            'receiver_id': 'Receiver',
+            'state': 'created',
+        }])
