c757e95fc16ad35b878e5032a16e4893adfcf0a5
leo-guggenberger <info@leo-guggenberger.at>
2017-05-31 10:35:27 +0200

    Update eai_client_scheduler.py

--- eai_client/eai_client_scheduler.py
+++ eai_client/eai_client_scheduler.py
@@ -10,4 +10,7 @@ class eai_client_scheduler(models.Model):
     _description = 'EAI Client Scheduler'
 
     def run_scheduler(self, cr, uid, context=None):
-
+        company_id = self.pool.get('res.users').browse(cr, uid, uid, context=context).company_id.id
+        
+        # Step 1: Produkte ausspielen
+        product_ids = self.pool['product.template'].search(cr,uid, ['|',('company_id','=',company_id),('active','=','true'),('sale_ok','=','true')], context=context)
