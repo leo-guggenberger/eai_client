09a64094cb05e8412d3f3a02d675dcb10d21b850
leo-guggenberger <info@leo-guggenberger.at>
2017-05-31 11:43:43 +0200

    Update eai_client_scheduler.py

--- eai_client/eai_client_scheduler.py
+++ eai_client/eai_client_scheduler.py
@@ -12,7 +12,10 @@ class eai_client_scheduler(models.Model):
 
     def run_scheduler(self, cr, uid, context=None):
         company_id = self.pool.get('res.users').browse(cr, uid, uid, context=context).company_id.id
-        eai_server_url = self.pool.get('res.users').browse(cr, uid, uid, context=context).company_id.id
+        eai_server_url = self.pool.get('eai_client.config.settings').browse(cr, uid, uid, context=context).eai_server_url
+        eai_server_db = self.pool.get('eai_client.config.settings').browse(cr, uid, uid, context=context).eai_server_db
+        eai_server_user = self.pool.get('eai_client.config.settings').browse(cr, uid, uid, context=context).eai_server_user
+        eai_server_password = self.pool.get('eai_client.config.settings').browse(cr, uid, uid, context=context).eai_server_password
         
         # Step 1: Produkte ausspielen
         #product_ids = self.pool['product.template'].search(cr,uid, ['|',('company_id','=',company_id),('active','=','true'),('sale_ok','=','true')], context=context)
