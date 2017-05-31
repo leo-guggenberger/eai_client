124f983036f495021c6dc5333029974a7ec0d179
leo-guggenberger <info@leo-guggenberger.at>
2017-05-31 09:36:30 +0200

    Create __openerp__.py

---
+++ eai_client/__openerp__.py
@@ -0,0 +1,27 @@
+# __openerp__.py
+# -*- coding: utf-8 -*-
+{
+    'name': "Enterprise Application Integration Client",
+    'version': '1.00',
+    'description': """
+Enterprise Application Integration Client
+=============
+        
+Enterprise Application Integration Client with following features:
+-------------------------------------
+* ...
+* Language support: English, German
+* Odoo version support: v8
+        """,
+    'author': 'Leo',
+    'website': 'http://www.leo-guggenberger.at',
+    'category': 'Generic Modules/Others',
+    'summary': 'Enterprise Application Integration Client for Odoo',
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'qweb': [],
+    'js': [],
+    'installable': True,
+    'auto_install': False,
+}
