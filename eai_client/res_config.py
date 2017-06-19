910782c272560b2d86623d962c37937b94b84e3d
leo-guggenberger <info@leo-guggenberger.at>
2017-06-19 14:33:15 +0200

    Update res_config.py

--- eai_client/res_config.py
+++ eai_client/res_config.py
@@ -8,6 +8,7 @@ class eai_client_config_settings(models.Model):
     _name = 'eai_client.config.settings'
     _inherit = 'res.config.settings'
     
+    eai_client_name = fields.Char('EAI Client Name')
     eai_server_url = fields.Char('EAI Server Url')
     eai_server_db = fields.Char('EAI Server Database')
     eai_server_username = fields.Char('EAI Server User'), default='admin'
